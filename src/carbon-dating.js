463276b236f1550fe4342c16260fd96097f45f2b
Kostyan91 <kostyan912404@gmail.com>
2022-04-26 13:45:06 +0300

    feat:add ggg

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,7 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
+function dateSample( sampleActivity ) {
   let  HALF_LIFE_PERIOD = 5730;
     let  MODERN_ACTIVITY = 15 ;
   
