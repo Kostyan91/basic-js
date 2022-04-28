eba4fa7d341e2b82b721cce606dc8195d9d87196
Kostyan91 <kostyan912404@gmail.com>
2022-04-28 20:23:26 +0300

    feat:add gcccccgf

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -13,15 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
 function getEmailDomain( email ) {
   // for(let i = 0; i <= email.length; i++) {
   //   if(email[i] === '@'){
-  //    return   email.substring(i+1)
+     return   email.slice(email.lastIndexOf('@')+1)
          
-  //   }
+    }
     
   //   }
-  const emails = email.split('@');
-  return emails[emails.length - 1];
+  // const emails = email.split('@');
+  // return emails[emails.length - 1];
     
-  }
+  // }
 
 module.exports = {
   getEmailDomain
